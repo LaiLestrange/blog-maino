f640e04a213a05a3d5769513dc98b0656b1736cf
Lais Almeida <laisdev25@gmail.com>
2024-12-29 21:41:13 -0300

    adjust devise views

--- spec/system/blogger_sign_up_spec.rb
+++ spec/system/blogger_sign_up_spec.rb
@@ -3,19 +3,21 @@ require 'rails_helper'
 describe 'Blogger signs up' do
   it 'successfully' do
     visit root_path
-    click_on 'Login'
+    click_on 'Entrar'
     click_on 'Cadastrar-se'
     fill_in 'Usuário', with: 'talsfulano'
     fill_in 'Nome', with: 'Fulano de Tal'
     fill_in 'E-mail', with: 'fulano@tal.com'
     fill_in 'Senha', with: 'p4ssw0rd'
-    fill_in 'Confirmar senha', with: 'p4ssw0rd'
-    click_on 'Criar conta'
+    fill_in 'Confirme a senha', with: 'p4ssw0rd'
+    click_on 'Cadastrar-se'
 
-    expect(page).to have_content 'Bem vindo, Fulano de Tal! Sua conta foi criada com sucesso!'
+    expect(page).to have_content 'Cadastro realizado com sucesso.'
+    expect(page).to have_button 'Sair'
+    expect(page).not_to have_button 'Entrar'
+
     expect(page).to have_content 'Fulano de Tal'
     expect(page).to have_content 'fulano@tal.com'
     expect(page).to have_content 'talsfulano'
-    expect(page).to have_button 'Sair'
   end
 end
