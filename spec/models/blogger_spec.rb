53c140b3b2f2fc9937dae9d7acb21c5b9f9c094b
Lais Almeida <laisdev25@gmail.com>
2024-12-30 00:39:28 -0300

    fix rubocop offense

--- spec/models/blogger_spec.rb
+++ spec/models/blogger_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Blogger, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
-    
+
     it { should validate_length_of(:fullname) }
     it { should validate_length_of(:username) }
     it { should validate_presence_of(:username) }
