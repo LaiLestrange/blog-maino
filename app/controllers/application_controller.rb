76f17230257460944b137ecbcd9ca786d7fe8dee
Lais Almeida <laisdev25@gmail.com>
2024-12-31 02:57:41 -0300

    sanitize strong devise parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :fullname])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :fullname])
   end
 end
--- spec/system/blogger/blogger_edit_account_spec.rb
+++ spec/system/blogger/blogger_edit_account_spec.rb
@@ -2,19 +2,16 @@ require 'rails_helper'
 
 describe 'Blogger edits account' do
   it 'successfully' do
-    blogger = create(:blogger,
-                     username: 'blogger2024',
-                     fullname: 'Blogger Girl')
+    blogger = create(:blogger, username: 'blogger2024')
 
     login_as blogger
     visit edit_blogger_registration_path
     click_on 'blogger2024'
-    fill_in 'Nome', with: 'Gossip Girl xoxo'
     fill_in 'Usuário', with: 'gossiper2025'
+    fill_in 'Senha atual', with: blogger.password
     click_on 'Atualizar'
 
     expect(page).to have_content 'Cadastro atualizado com sucesso'
     expect(page).to have_content 'gossiper2025'
-    expect(current_path).to eq blogger_path blogger
   end
 end
