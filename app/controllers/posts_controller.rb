6a75ad971d6aeb79b3c10c9f4c3ec7cb1774bbb3
Lais Almeida <laisdev25@gmail.com>
2025-01-03 18:31:37 -0300

    blogger can create a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,35 @@
 class PostsController < ApplicationController
+  before_action :authenticate_blogger!
+  before_action :set_blogger, only: [:new, :create]
+  before_action :set_post, only: [:show]
+  def show; end
+
   def new
-    @post = Post.new
+    @post = Post.new(blogger: @blogger)
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.blogger = @blogger
+    if @post.save
+      redirect_to post_path(@post), notice: I18n.t('app.created_post')
+    else
+      flash.now[:notice] = I18n.t('post.cant_post')
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_blogger
+    @blogger = current_blogger
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:text)
   end
 end
--- spec/system/post/blogger_creates_post_spec.rb
+++ spec/system/post/blogger_creates_post_spec.rb
@@ -9,9 +9,8 @@ describe 'Blogger creates post' do
     click_on 'Novo post'
     fill_in 'post', with: 'E aí pessoal! Esse é meu primeiro post!'
     click_on 'Enviar'
-    # save_and_open_page
 
-    expect(page).to have_content 'E aí pessoal! Esse é o meu primeiro post!'
+    expect(page).to have_content 'E aí pessoal! Esse é meu primeiro post!'
     expect(page).to have_content 'Postado com sucesso!'
     expect(page).not_to have_button 'Enviar'
   end
