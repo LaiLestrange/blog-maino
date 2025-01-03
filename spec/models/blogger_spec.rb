9175d83dcd401c5ef2c018518ff3506b4253d8da
Lais Almeida <laisdev25@gmail.com>
2025-01-03 17:33:37 -0300

    fix blogger spec shouldamatchers usage

--- spec/models/blogger_spec.rb
+++ spec/models/blogger_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Blogger, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
 
-    it { should validate_length_of(:fullname) }
-    it { should validate_length_of(:username) }
+    it { should validate_length_of(:username).is_at_least(4).is_at_most(15) }
+    it { should validate_length_of(:fullname).is_at_least(4).is_at_most(50) }
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username) }
   end
