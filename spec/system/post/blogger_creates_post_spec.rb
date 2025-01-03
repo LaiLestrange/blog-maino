55c8610fa8879b28e6ac64f49fe7e687985c6377
Lais Almeida <laisdev25@gmail.com>
2025-01-03 17:17:11 -0300

    new post form

--- spec/system/post/blogger_creates_post_spec.rb
+++ spec/system/post/blogger_creates_post_spec.rb
@@ -6,9 +6,10 @@ describe 'Blogger creates post' do
     login_as blogger
 
     visit root_path
-    click_on 'Novo Post'
-    fill_in 'Post', with: 'E aí pessoal! Esse é meu primeiro post!'
+    click_on 'Novo post'
+    fill_in 'post', with: 'E aí pessoal! Esse é meu primeiro post!'
     click_on 'Enviar'
+    save_and_open_page
 
     expect(page).to have_content 'E aí pessoal! Esse é o meu primeiro post!'
     expect(page).to have_content 'Postado com sucesso!'
