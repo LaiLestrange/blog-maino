eb47305d926dcd3ffc75c1d4111afe2a19860473
Lais Almeida <laisdev25@gmail.com>
2025-01-04 05:24:17 -0300

    blogger can edit post

--- spec/system/post/blogger_edits_post_spec.rb
+++ spec/system/post/blogger_edits_post_spec.rb
@@ -5,16 +5,20 @@ describe 'Blogger edits post' do
     blogger = create(:blogger)
     post = create(:post, blogger: blogger, text: 'original text')
 
+    byebug
+
     login_as blogger
     visit post_path(post)
     find('.edit-post-btn').click
-    fill_in 'Post', with: 'edited text'
+    fill_in 'post_text', with: 'edited text'
     click_on 'Atualizar'
+
+    byebug
 
     expect(current_path).to eq post_path(post)
     expect(page).not_to have_content 'original text'
     expect(page).to have_content 'edited text'
-    expect(page).to have_content 'Post editado com sucesso!'
+    expect(page).to have_content 'Atualizado com sucesso!'
     expect(page).to have_content '(Editado)'
   end
 end
