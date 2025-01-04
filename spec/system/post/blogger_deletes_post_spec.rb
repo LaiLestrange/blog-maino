3b35c4d692ba00b6859dec3c5246847d2682b58c
Lais Almeida <laisdev25@gmail.com>
2025-01-04 16:52:24 -0300

    adds button to delete post

---
+++ spec/system/post/blogger_deletes_post_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Blogger deletes post' do
+  it 'from the post page', js: true do
+    blogger = create(:blogger)
+    post = create(:post, blogger: blogger, text: 'original text')
+
+    login_as blogger
+    visit post_path(post)
+    accept_confirm do
+      find('.delete-post-btn').click
+    end
+
+    expect(page).to have_content 'Post deletado com sucesso!'
+  end
+end
+
+describe 'Blogger deletes post' do
+  it 'from the edit_post page', js: true do
+    blogger = create(:blogger)
+    post = create(:post, blogger: blogger, text: 'original text')
+
+    login_as blogger
+    visit edit_post_path(post)
+    accept_confirm do
+      click_on 'Deletar'
+    end
+
+    expect(page).to have_content 'Post deletado com sucesso!'
+  end
+end
