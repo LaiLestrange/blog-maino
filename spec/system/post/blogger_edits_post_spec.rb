2e2bfd69dbd6748282120341aa29c9d48d2d5813
Lais Almeida <laisdev25@gmail.com>
2025-01-04 04:32:07 -0300

    blogger edits post

---
+++ spec/system/post/blogger_edits_post_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'Blogger edits post' do
+  it 'successfully' do
+    blogger = create(:blogger)
+    post = create(:post, blogger: blogger, text: 'original text')
+
+    login_as blogger
+    visit post_path(post)
+    click_on 'Editar'
+    fill_in 'Post', with: 'edited text'
+    click_on 'Atualizar'
+
+    expect(current_path).to eq post_path(post)
+    expect(page).not_to have_content 'original text'
+    expect(page).to have_content 'edited text'
+    expect(page).to have_content 'Post editado com sucesso!'
+    expect(page).to have_content '(Editado)'
+  end
+end
