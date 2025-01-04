05ac0f3812bc3d1092902d2b81222f02c3a77b49
Lais Almeida <laisdev25@gmail.com>
2025-01-04 05:36:15 -0300

    add status enum to posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Post, type: :model do
     it { should belong_to(:blogger) }
     it { should validate_presence_of(:text) }
     it { should validate_length_of(:text).is_at_most(140) }
+    it { should define_enum_for(:status).with_default(:posted) }
   end
 end
--- spec/system/post/blogger_edits_post_spec.rb
+++ spec/system/post/blogger_edits_post_spec.rb
@@ -5,15 +5,11 @@ describe 'Blogger edits post' do
     blogger = create(:blogger)
     post = create(:post, blogger: blogger, text: 'original text')
 
-    byebug
-
     login_as blogger
     visit post_path(post)
     find('.edit-post-btn').click
     fill_in 'post_text', with: 'edited text'
     click_on 'Atualizar'
-
-    byebug
 
     expect(current_path).to eq post_path(post)
     expect(page).not_to have_content 'original text'
