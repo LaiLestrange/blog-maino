17e0b203edff251706c0727d72c7adb567c31b19
Lais Almeida <laisdev25@gmail.com>
2025-01-05 19:04:44 -0300

    adds pagination with order and filtering deleted ones

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.where.not(status: :deleted).order(created_at: :desc).page(params[:page]).per(3)
   end
 end
