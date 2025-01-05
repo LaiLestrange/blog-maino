71ed1ab4c952e49b12fcaa27a93f6cfe4c6d9d7e
Lais Almeida <laisdev25@gmail.com>
2025-01-05 18:08:57 -0300

    shows all posts on index page

--- spec/system/visitor_view_home_spec.rb
+++ spec/system/visitor_view_home_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 describe 'Visitor views home page' do
-  it 'successfully' do
+  it 'and sees latest posts' do
+    create(:post, text: 'um post para aparecer na home_page')
+
     visit root_path
+
     expect(page).to have_content('Blog Mainô')
+    expect(page).to have_content('Posts mais recentes')
+    expect(page).to have_content('um post para aparecer na home_page')
   end
 end
