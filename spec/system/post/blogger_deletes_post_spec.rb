1c280f0b49d3d5a66a8b77ffa732241b994c6c53
Lais Almeida <laisdev25@gmail.com>
2025-01-04 23:10:15 -0300

    adjust layout

--- spec/system/post/blogger_deletes_post_spec.rb
+++ spec/system/post/blogger_deletes_post_spec.rb
@@ -3,27 +3,27 @@ require 'rails_helper'
 describe 'Blogger deletes post' do
   it 'from the post page' do
     blogger = create(:blogger)
-    post = create(:post, blogger: blogger, text: 'original text')
+    post = create(:post, blogger: blogger, text: 'Post que será deletado')
 
     login_as blogger
     visit post_path(post)
     find('.delete-post-btn').click
 
     expect(page).to have_content 'Post deletado com sucesso!'
+    expect(page).not_to have_content 'Post que será deletado'
   end
 end
 
-# describe 'Blogger deletes post' do
-#   it 'from the edit_post page', js: true do
-#     blogger = create(:blogger)
-#     post = create(:post, blogger: blogger, text: 'original text')
+describe 'Blogger deletes post' do
+  it 'from the edit_post page', js: true do
+    blogger = create(:blogger)
+    post = create(:post, blogger: blogger, text: 'Post que será deletado')
 
-#     login_as blogger
-#     visit edit_post_path(post)
-#     accept_confirm do
-#       click_on 'Deletar'
-#     end
+    login_as blogger
+    visit edit_post_path(post)
+    click_on 'Deletar'
 
-#     expect(page).to have_content 'Post deletado com sucesso!'
-#   end
-# end
+    expect(page).to have_content 'Post deletado com sucesso!'
+    expect(page).not_to have_content 'Post que será deletado'
+  end
+end
