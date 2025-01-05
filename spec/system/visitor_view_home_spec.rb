574fc3f5963acb3e62bfff1937cdc1fb971f833b
Lais Almeida <laisdev25@gmail.com>
2025-01-05 19:26:21 -0300

    tests 3 per page

--- spec/system/visitor_view_home_spec.rb
+++ spec/system/visitor_view_home_spec.rb
@@ -10,4 +10,15 @@ describe 'Visitor views home page' do
     expect(page).to have_content('Posts mais recentes')
     expect(page).to have_content('um post para aparecer na home_page')
   end
+
+  it 'and sees 3 posts per page' do
+    create_list(:post, 14, text: 'posts para paginação')
+
+    visit root_path
+    within '.pagination' do
+      click_link 'última'
+    end
+
+    expect(page).to have_css('.feed-post', count: 2)
+  end
 end
